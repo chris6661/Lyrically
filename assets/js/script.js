225a99d5a8e55fdc4ce84a25966c70804f9e2297
Dax Buratto <daxburatto@gmail.com>
2021-01-22 15:09:04 -0600

    mvp script for song lyrics

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var songSearchEl = document.querySelector("#song")
+var songId = document.querySelector("#songId")
+var artistSearchEl = document.querySelector("#artist")
+var songLyricsEl = document.querySelector("#songLyrics")
+var lyricsDisplay = document.querySelector("#lyricsDisplay")
+
+var formSubmitHandler = function (event) {  
+    event.preventDefault()
+
+    var song = songSearchEl.value.trim()
+    var artist = artistSearchEl.value.trim()
+
+    if (song, artist) {
+        songSearch(song, artist)
+        songSearchEl.value = ""
+        artistSearchEl.value = ""
+    } else {
+        alert("Please Enter A Song Name")
+    }
+    console.log(song)
+    console.log(artist)
+}
+
+var songSearch = function (song, artist) { 
+    console.log(song, artist)
+    var apiURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song
+    console.log(apiURL)
+    fetch(apiURL).then(function (response) {  
+        if (response.ok) {
+            response.json().then(function (data) {  
+                console.log(data.lyrics)
+                displayLyrics(data)
+            })
+        }
+    })
+}
+
+var displayLyrics = function (data) {
+    lyricsDisplay.textContent = data.lyrics
+}
+
+
+songId.addEventListener("submit", formSubmitHandler)

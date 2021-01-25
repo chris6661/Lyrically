41abd6b7e1b1290ca15bab38fbcdc36cbb6671ca
Paden Allen <padenallen15@yahoo.com>
2021-01-25 16:43:55 -0600

    youtube videos now correctly display the inputted song/artist

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,21 @@ var artist = "";
 var formSubmitHandler = function (event) {  
     event.preventDefault();
 
-    var song = songSearchEl.value.trim();
-    var artist = artistSearchEl.value.trim();
+    song = songSearchEl.value.trim();
+    artist = artistSearchEl.value.trim();
 
     if (song, artist) {
         songSearch(song, artist);
-        songInfo.textContent = song + " by " + artist;
+        songInfo.textContent = song.toUpperCase() + " BY " + artist.toUpperCase();
         songSearchEl.value = "";
         artistSearchEl.value = "";
     } else {
         alert("Please Enter A Song Title And Artist Name");
     }
-    console.log(song);
-    console.log(artist);
 }
 
 var songSearch = function (song, artist) { 
-    console.log(song, artist);
     var apiURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song;
-    console.log(apiURL);
     fetch(apiURL).then(function (response) {  
         if (response.ok) {
             response.json().then(function (data) {  
@@ -48,26 +44,26 @@ var displayLyrics = function (data) {
         console.log("no lyrics found");
         lyricsDisplay.textContent = "No Lyrics Found";
         return;
-    }
-    lyricsDisplay.textContent = "";
-    lyricsDisplay.textContent = data.lyrics;
-    // YOUTUBE
-var YoutubeApi = "AIzaSyDmjBtZUutKjG2725gC5lcGoV_jzDwbR7o";
-
-
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=kane brown heaven&type=video&videoCaption=closedCaption&key=" + YoutubeApi)
-
-.then(function (response) {  
-    if (response.ok) {
-        response.json().then(function (data) {  
-            console.log(data);
-        })
     } else {
-        alert("Error: " + response.statusText);
+        lyricsDisplay.textContent = "";
+        lyricsDisplay.textContent = data.lyrics;
+        
+        // YOUTUBE
+        var YoutubeApi = "AIzaSyDmjBtZUutKjG2725gC5lcGoV_jzDwbR7o";
+        fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" + song + " " + artist + "&type=video&videoCaption=closedCaption&key=" + YoutubeApi)
+
+        .then(function (response) {  
+            if (response.ok) {
+                response.json().then(function (data) {  
+                    //console.log(data);
+                    var videoId = data.items[0].id.videoId;
+                    document.getElementById("video").src = "https://www.youtube.com/embed/" + videoId;
+                })
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
     }
-})
 }
 
 searchSongBtn.addEventListener("click", formSubmitHandler);
-
-

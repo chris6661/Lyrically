cc356e53f14076abf66e75a21d957dbc9ebff6c2
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-01-25 15:41:00 -0600

    video search working, styled basically just  for html usage and testing

--- README.md
+++ README.md
@@ -1,29 +1,19 @@
 # Cyrical
-## Help the user understand the meaning behind the lyrics from either the artists as well as gain other user perspectives; help user learn lyrics for songs hey are listening to. 
+## This application was created using the YouTube API to search for videos and the Genius API to search for the accompanying song lyrics. One of the requirements for this project was to use a different framework other than Bootstrap for stlying, and we chose Bulma instead. Additionally, we chose to use NES CSS style sheet for some added flare and style.
+
+## The purpose of this application is to let a user search for a video and play it on the same page, while also displaying the lyrics of the video to the right so the user is able to follow along with the video. This was to help the user better learn the lyrics to the song they searched for, as well as gain a better understanding of what the song lyrics mean and gain a deeeper understanding of the music. 
+
 ### Built With 
 
  * HTML
+ * NES CSS, an NES style framework
  * CSS
- * 
+ * Bulma
  * jQuery
  * JavaScript
 
-
-1.What does the MVP actually look like functionality wise?
-video playing, lyrics play/roll with video, mobile responsive
-2.What should name be? For Paden and Sunsia, what other use for the user do you think this could have other than it is listed above?
-
-3.Review the 'mock up', is there anything else we want to add to the page for version 1.0?
---section of lyrics randomied word is blanked out, user has to enter lyric or word to fill out; every (ex. 15th word) blanked out?
-4..Where does everyones strength lie? 
-Padan: framework/layout;
-Sunisa: JavaScript for Genius; 
-Dax: API;
-Chris: JavaScript for spotify;
-5.What parts need to be done first, will anything not being done impact anyone else's work?
-
-6.Does it make sense to wait until next wedensday 1/27 to have mvp done and ready to rollout, or should we push ourselves to stay on the usual time frame (mvp 100% done by 1/24) for extra timwe on 1/25 to work on it and play around with it, maybe add a new feature?
--main concepts for mvp by 1/24; styling by 1/27;
-7.How quickly can we get the mvp out? What does it look like timeline wise for us to be able to meet up and discuss any changes? 
-
-8.What CSS framework should we use since we can not use bootstrap? Will this affect anyone else, or just the person doing the stlying? !!need to make sure it is tagged to the HTML file correctly (refer to bootstrap link)!!
+## Contributors
+* padenallen15
+* daxburrato
+* xdaedx
+* chris6661
--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,24 +1,146 @@
-// when i type in an song/artists name in the search BarProp, the music plays in the spotify side and the lyrics are displayed in the box next to it and they play at the same time
+//youtube api AIzaSyDmjBtZUutKjG2725gC5lcGoV_jzDwbR7o
+// client id 55292436491-s66k5pgmu4n07e81b4s4fbdgv7bhpkbs.apps.googleusercontent.com
+//client secret 65I8xJeTlyRBKotP1AcCwjjg
 
-// --functions you will need!!!--
-// --search bar takes text input and pulls song/artist from spotify
-// --song plays when play button is clicked, paused when pause button is clicked
-// --scrub function for when songs are playing
-// --error for search box if empty entry 
-// --local storage for people who search for things
-// --api to pull info from spotify
-// --albun/song cover from spotify to fill box //spotify logo based off time constraints
+$(document).ready(function(){
 
-const pressed = [];
-const secretCode = 'coding';
+	(function(){
 
-window.addEventListener('keyup', (e) => {
-    console.log(e.key);
-    pressed.push(e.key);
-    pressed.splice(-secretCode.length-1, pressed.length - secretCode.length);
-    if (pressed.join('').includes(secretCode)) {
-      console.log('DING DING!');
-      cornify_add(); //adds unicorns at random lolololol
-    }
-    console.log(pressed);
-  }); 
+		// variables
+		const $searchTxt = $("#searchTxt");
+		const $results = $("#results");
+		const $searchBtn = $("#searchBtn");
+		const youtubeURL = "https://www.googleapis.com/youtube/v3/search";
+		const $collection = $(".collection");
+
+
+		//$results.fadeIn(3000);
+
+		// Search button event listener
+		$searchBtn.on("click", function(){
+			
+			let query = $searchTxt.val();
+
+			//check if query len is more than 3 chars
+			if (query.length > 3){
+				// when the search button is clicked, 
+				// take the search text box value and call the handleSearch()
+				// passing in the query
+				
+				$results.fadeIn(5000);
+
+				handleSearch(query);
+			} else {
+				$searchTxt.focus();
+			}
+
+		});
+
+		function handleSearch(query){
+			
+			// jQuery GET request
+			// hitting the Youtube URL 
+            // sending the request obj with the query included
+            
+
+                    //might need changed based off how many options want to display!!
+			$.get(youtubeURL, {'maxResults': '3',
+				'part': 'snippet',
+				'q': query,
+				'type': '',
+				key: 'AIzaSyDmjBtZUutKjG2725gC5lcGoV_jzDwbR7o'
+			}, 
+			function(data){
+	     	
+	     	handleData(data);
+     	}) // end of $.get
+
+		}; // end of handleSearch()
+
+		// JSON data passed in
+		// take the data and plug into HTML
+		function handleData(data) {
+
+			// clearing the items inside
+			// when performing second search
+			$results.html = "";
+
+			// loop through all the data obj
+			data.items.forEach((currentValue, index, array)=>{
+
+				// find the 
+				// [0].id.videoId
+				// [0].snippet.title
+				// [0].snippet.description
+				// [0].snippet.thumbnails.medium.url				
+
+				let vidID = currentValue.id.videoId;
+				let channelId = currentValue.snippet.channelId;
+				let title = currentValue.snippet.title;
+				let creator = currentValue.snippet.channelTitle;
+				let uploadDateTime = currentValue.snippet.publishedAt;
+				//get only the date
+				//ignore the time
+				let uploadDateOnly = uploadDateTime.slice(0, uploadDateTime.indexOf("T"));
+				let desc = currentValue.snippet.description;
+				let imageURL = currentValue.snippet.thumbnails.medium.url;
+
+				//call the generateHTML passing in required info
+				generateHTML(vidID, channelId, title, creator, uploadDateOnly, desc, imageURL)
+
+			}) 
+
+		}; 
+
+		function generateHTML(vidID, channelId, title, creator, uploadDate, desc, imageUrl){
+
+			let videoUrl = "https://www.youtube.com/watch?v=" + vidID;
+			let creatorUrl = "https://www.youtube.com/channel/" + channelId;
+			let HTML_TEMPLATE = '<li class="collection-item">' +
+						'<a href="' +
+						videoUrl +
+						'">' +
+						'<div class="image-container">' +
+						'<img class="image" src="' +
+						imageUrl +
+						'" alt="">' +
+						'</div>' +
+						'</a>' +
+						'<div class="video-info-container">' +
+						'<ul class="video-info">' +
+						'<li class="title"><a href="' +
+						videoUrl +
+						'">' + 
+						title +
+						'</a></li>' +
+						'<li class="creator"><a href="' +
+						creatorUrl + 
+						'">' + 
+						creator +
+						'</a></li>' +
+						'<li class="upload-date">' +
+						uploadDate +
+						'</li>' +
+						'<li class="desc">' +
+						desc +
+						'</li>' +
+						'</ul>' +
+						'</div>' +
+						'</li>';
+
+		// add HTML template to results div
+		$results.append(HTML_TEMPLATE);
+
+		} 
+
+		let $testBtn = $("#testBtn").on("click", function(){
+			handleData(testData);
+		})
+
+	})(); 
+
+    
+
+}); 
+
+

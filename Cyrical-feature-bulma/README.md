3f67d8efc749f5c983265cc7ffed69afce4fc96e
xdaedx <hewittsunisa@gmail.com>
2021-01-25 21:00:35 -0600

    Nes CSS

---
+++ Cyrical-feature-bulma/README.md
@@ -0,0 +1,29 @@
+# Cyrical
+## Help the user understand the meaning behind the lyrics from either the artists as well as gain other user perspectives; help user learn lyrics for songs they are listening to.
+### Built With 
+
+ * HTML
+ * CSS
+ * Foundation instead of Bootstrap?
+ * jQuery
+ * JavaScript
+
+
+1.What does the MVP actually look like functionality wise?
+
+2.What should name be? For Paden and Sunsia, what other use for the user do you think this could have other than it is listed above?
+* Name ideas: Lyrically, Lyripedia, Lyrify, Songify 
+* Learn a new language - ability to click on a word in the lyrics and the definition pops up and maybe also with a picture?
+* Memorization game - start the song but have random blanks in the lyrics that the user has to input to test if they have the song memorized
+
+3.Review the 'mock up', is there anything else we want to add to the page for version 1.0?
+
+4..Where does everyones strength lie? 
+
+5.What parts need to be done first, will anything not being done impact anyone else's work?
+
+6.Does it make sense to wait until next wedensday 1/27 to have mvp done and ready to rollout, or should we push ourselves to stay on the usual time frame (mvp 100% done by 1/24) for extra timwe on 1/25 to work on it and play around with it, maybe add a new feature?
+
+
+7.How quickly can we get the mvp out? What does it look like timeline wise for us to be able to meet up and discuss any changes? 
+
---
+++ Cyrical-feature-bulma/assets/js/script.js
@@ -0,0 +1,73 @@
+var songSearchEl = document.querySelector("#song");
+var artistSearchEl = document.querySelector("#artist");
+var searchSongBtn = document.querySelector("#searchSong");
+var songInfo = document.querySelector("#songInfo");
+var lyricsDisplay = document.querySelector("#lyricsDisplay");
+var song = "";
+var artist = "";
+
+var formSubmitHandler = function (event) {  
+    event.preventDefault();
+
+    var song = songSearchEl.value.trim();
+    var artist = artistSearchEl.value.trim();
+
+    if (song, artist) {
+        songSearch(song, artist);
+        songInfo.textContent = song + " by " + artist;
+        songSearchEl.value = "";
+        artistSearchEl.value = "";
+    } else {
+        alert("Please Enter A Song Title And Artist Name");
+    }
+    console.log(song);
+    console.log(artist);
+}
+
+var songSearch = function (song, artist) { 
+    console.log(song, artist);
+    var apiURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song;
+    console.log(apiURL);
+    fetch(apiURL).then(function (response) {  
+        if (response.ok) {
+            response.json().then(function (data) {  
+                console.log(data.lyrics);
+                displayLyrics(data);
+            })
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function (event) {  
+        alert("Unable to connect to Lyrics API");
+    })
+}
+
+var displayLyrics = function (data) {
+    if (data.lyrics.length === 0) {
+        console.log("no lyrics found");
+        lyricsDisplay.textContent = "No Lyrics Found";
+        return;
+    }
+    lyricsDisplay.textContent = "";
+    lyricsDisplay.textContent = data.lyrics;
+    // YOUTUBE
+var YoutubeApi = "AIzaSyDmjBtZUutKjG2725gC5lcGoV_jzDwbR7o";
+
+
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=kane brown heaven&type=video&videoCaption=closedCaption&key=" + YoutubeApi)
+
+.then(function (response) {  
+    if (response.ok) {
+        response.json().then(function (data) {  
+            console.log(data);
+        })
+    } else {
+        alert("Error: " + response.statusText);
+    }
+})
+}
+
+searchSongBtn.addEventListener("click", formSubmitHandler);
+
+

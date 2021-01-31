ff1895d8b5cfd981689015468c1b2e035bc56b91
Paden Allen <padenallen15@yahoo.com>
2021-01-31 11:33:03 -0600

    added search history feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,14 @@ var artistSearchEl = document.querySelector("#artist");
 var searchSongBtn = document.querySelector("#searchSong");
 var songInfo = document.querySelector("#songInfo");
 var lyricsDisplay = document.querySelector("#lyricsDisplay");
+var searchContainer = document.querySelector("#searchHistory");
+var showHistoryBtn = document.querySelector("#showHistory");
+var historyLog = [];
+var conditional = "";
 var song = "";
 var artist = "";
 var testLyric = "";
+var counter = 1;
 
 if (localStorage.getItem("lyrics") != null) {
     document.getElementById("lyricsDisplay").innerHTML = localStorage.getItem("lyrics");
@@ -22,8 +27,6 @@ var formSubmitHandler = function (event) {
 
     if (song, artist) {
         songSearch(song, artist);
-        songInfo.textContent = song.toUpperCase() + " BY " + artist.toUpperCase();
-        localStorage.setItem("songInfo", song.toUpperCase() + " BY " + artist.toUpperCase());
         songSearchEl.value = "";
         artistSearchEl.value = "";
     } else {
@@ -49,7 +52,9 @@ var songSearch = function (song, artist) {
     var apiURL = "https://api.lyrics.ovh/v1/" + artist + "/" + song;
     fetch(apiURL).then(function (response) {  
         if (response.ok) {
-            response.json().then(function (data) {  
+            response.json().then(function (data) { 
+                songInfo.textContent = song.toUpperCase() + " BY " + artist.toUpperCase();
+                localStorage.setItem("songInfo", song.toUpperCase() + " BY " + artist.toUpperCase()); 
                 displayLyrics(data);
             })
         } else {
@@ -70,8 +75,6 @@ var displayLyrics = function (data) {
     } else {
         lyricsDisplay.textContent = "";
         var lyricText = data.lyrics.split("\n");
-        console.log(data.lyrics);
-        console.log(lyricText);
         
         for (var i =0; i < lyricText.length; i++) {
             lyricText[i] = lyricText[i] + "<br>";
@@ -87,10 +90,10 @@ var displayLyrics = function (data) {
         .then(function (response) {  
             if (response.ok) {
                 response.json().then(function (data) {  
-                    //console.log(data);
                     var videoId = data.items[0].id.videoId;
                     document.getElementById("video").src = "https://www.youtube.com/embed/" + videoId;
                     localStorage.setItem("video", "https://www.youtube.com/embed/" + videoId);
+                    searchHistoy();
                 })
             } else {
                 alert("Error: " + response.statusText);
@@ -99,4 +102,65 @@ var displayLyrics = function (data) {
     }
 }
 
+// Creates search history log
+var searchHistoy = function(){
+    // Determining if previous search is already in the history log
+    if (historyLog.includes(localStorage.getItem("songInfo"))){
+        conditional = "yes";
+    } else {
+        conditional = "no"
+        historyLog.push(localStorage.getItem("songInfo"));
+    }
+    
+    // If previous search is not in the history log, create new entry
+    if (historyLog.length === 0 || conditional === "no"){
+        var historyField = document.createElement("div");
+        historyField.className = "historyField field has-addons";
+        historyField.style.height = "41px";
+        historyField.style.maxWidth = "inherit";
+        var historySongField = document.createElement("p");
+        historySongField.className = "control";
+        var historySongBtn = document.createElement("a");
+        historySongBtn.className = "historyBtn button is-primary is-outlined";
+        historySongBtn.textContent = localStorage.getItem("songInfo");
+        historySongField.appendChild(historySongBtn);
+        var deleteField = document.createElement("p");
+        deleteField.className = "control";
+        var deleteBtn = document.createElement("a");
+        deleteBtn.className = "deleteBtn button is-danger is-outlined";
+        deleteBtn.textContent = "X";
+        deleteField.appendChild(deleteBtn);
+        historyField.appendChild(historySongField);
+        historyField.appendChild(deleteField);
+        searchContainer.appendChild(historyField);
+    } 
+}
+
+// If a song in search history is clicked reload that song
+$("#searchHistory").on("click", ".historyBtn", function(){
+    var split = this.textContent.split(" BY ");
+    song = split[0].trim();
+    artist = split[1].trim();
+    songSearch(song, artist);
+})
+
+// Removes a previous search from search history when delete button clicked
+$("#searchHistory").on("click", ".deleteBtn", function(){
+    $(this).closest(".historyField").remove();
+})
+
+// Hides or shows search history
+$("#showHistory").click(function(){
+    var $this = $(this);
+    if ($this.text() === "Show Search History"){
+        $this.text("Hide Search History");
+        $("#searchHistory").show();
+        
+    } else {
+        $this.text("Show Search History");
+        $("#searchHistory").hide();
+    }
+});
+
+// when search button is clicked
 searchSongBtn.addEventListener("click", formSubmitHandler);
